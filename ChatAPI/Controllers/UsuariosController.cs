86ec48404c3c8de44d54c13b4b6931735d40b1c7
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-05-07 00:24:07 -0300

    Add docs for UsuarioController

--- ChatAPI/Controllers/UsuariosController.cs
+++ ChatAPI/Controllers/UsuariosController.cs
@@ -10,9 +10,16 @@ using ChatAPI.Models;
 
 namespace ChatAPI.Controllers
 {
+    /// <summary>
+    /// Controlador de objetos da classe Usuario.
+    /// </summary>
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsuariosController : ApiController
     {
+        /// <summary>
+        /// Pedido GET da lista de usuários.
+        /// </summary>
+        /// <returns>Lista de objetos da classe Usuario</returns>
         public IEnumerable<Usuario> Get()
         {
             using (UsuarioDBContext dbContext = new UsuarioDBContext())
@@ -28,6 +35,11 @@ namespace ChatAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Pedido GET de um usuário.
+        /// </summary>
+        /// <param name="id">RA do usuário</param>
+        /// <returns>Um objeto da classe Usuario</returns>
         public Usuario Get(int id)
         {
             using (UsuarioDBContext dbContext = new UsuarioDBContext())
@@ -40,12 +52,25 @@ namespace ChatAPI.Controllers
             }
         }
 
-        public void Post([FromBody]Usuario u)
+        /// <summary>
+        /// POST de cadastro de um usuário.
+        /// </summary>
+        /// <param name="u">Objeto da classe Usuário</param>
+        /// <returns>Se o cadastro foi bem-sucedido</returns>
+        public bool Post([FromBody]Usuario u)
         {
             using (UsuarioDBContext dbContext = new UsuarioDBContext())
             {
-                dbContext.Usuario.Add(u);
-                dbContext.SaveChanges();
+                try
+                {
+                    dbContext.Usuario.Add(u);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
             }
         }
     }
